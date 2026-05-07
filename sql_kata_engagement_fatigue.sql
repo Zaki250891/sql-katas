3618ccf835f67ff401bacf56bbdba3afb2026285
zak <dunozak@gmail.com>
2026-05-07 16:29:53 -0600

    correción-Self-Join

--- sql_kata_engagement_fatigue.sql
+++ sql_kata_engagement_fatigue.sql
@@ -16,7 +16,7 @@ SELECT
     s.channel,
     COUNT(DISTINCT e.id) AS engagements
 FROM sends current_sends
-LEFT JOIN sends previous_sends 
+SELF JOIN sends previous_sends 
 ON current_sends.user_id = previous_sends.user_id
 AND previous.sent_at < current.sent_at
 AND previous.sent_at >= current.sent_at - INTERVAL '7 days'
